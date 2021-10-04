873685b7eb19c928a3dcd82eebbf1d1906789e2d
Rory Killick <rorykillick@gmail.com>
2021-10-04 21:46:56 +0200

    updated theme numbers

--- calculator-app/src/Theme.js
+++ calculator-app/src/Theme.js
@@ -3,20 +3,27 @@ import React from 'react';
 function Theme() {
     return (
         <div className='theme'>
-            <p>Theme</p>
-            <div className='theme-toggle'>
-                <label className='custom-radio-button'>
-                    <input id='1' name='toggle-state' type='radio' defaultChecked/>
-                    <span className='checkmark'></span>
-                </label>
-                <label className='custom-radio-button'>
-                    <input id='2' name='toggle-state' type='radio'/>
-                    <span className='checkmark'></span>
-                </label>
-                <label className='custom-radio-button'>
-                    <input id='3' name='toggle-state' type='radio'/>
-                    <span className='checkmark'></span>
-                </label>
+            <p className='title'>Theme</p>
+            <div>
+                <div className='numbers'>
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                </div>
+                <div className='theme-toggle'>
+                    <label className='custom-radio-button'>
+                        <input id='1' name='toggle-state' type='radio' defaultChecked/>
+                        <span className='checkmark'></span>
+                    </label>
+                    <label className='custom-radio-button'>
+                        <input id='2' name='toggle-state' type='radio'/>
+                        <span className='checkmark'></span>
+                    </label>
+                    <label className='custom-radio-button'>
+                        <input id='3' name='toggle-state' type='radio'/>
+                        <span className='checkmark'></span>
+                    </label>
+                </div>
             </div>
         </div>
     )
